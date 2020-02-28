ddd4aaf8fd5ca508324cfccdbb3471574bfc15f4
Matthias Jouanneaux <mjoux@dlcluster.nvidia.com>
2020-02-28 07:06:00 -0800

    added Ubuntu installs of newer gcc instead of conda, removed legate from Docker itself

--- modules/legate_dask.py
+++ modules/legate_dask.py
@@ -9,29 +9,31 @@ def emit(writer, **kwargs):
     is_debug = kwargs.get('debug', False)
     legate_branch = kwargs.get('legate_branch', 'dask')
     print('Using debug build: ', is_debug, ' legate branch: ', legate_branch)
-    writer.packages(['zlib1g-dev'])
-    writer.condaPackages(['gxx_linux-64=7.3.0', 'gcc_linux-64=7.3.0', 'gfortran_linux-64=7.3.0', 'openblas-devel'],
-        channels=['anaconda', 'conda-forge'],
-        installOpts='-n rapids')
+    writer.packages(['software-properties-common', 'zlib1g-dev'])
+    writer.emit("""
+        RUN add-apt-repository ppa:ubuntu-toolchain-r/test
+    """)
+    writer.packages(['gcc-7', 'g++-7'])
+    writer.emit("""
+        RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 90 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+        ENV CC gcc-7
+        ENV CXX g++-7
+    """)
+    writer.condaPackages(['openblas-devel'], channels=['anaconda', 'conda-forge'], installOpts='-n rapids')
     writer.emit("""
         RUN git clone --branch v1.0.1 https://github.com/NVIDIA/cutlass.git /opt/cutlass
     """)
-    writer.emit("""
-        RUN ln -s /opt/conda/envs/rapids/bin/x86_64-conda_cos6-linux-gnu-gcc /usr/local/cuda/bin/gcc
-        RUN ln -s /opt/conda/envs/rapids/bin/x86_64-conda_cos6-linux-gnu-g++ /usr/local/cuda/bin/g++
-        RUN ln -s /opt/conda/envs/rapids/bin/x86_64-conda_cos6-linux-gnu-gfortran /usr/local/cuda/bin/gfortran
-    """)
-    writer.emit("""
-        RUN source activate rapids && \\
-            echo "Change this message for re-build!" && \\
-            git clone -b $legate_branch https://gitlab-master.nvidia.com/mjoux/Legate.git \\
-            /opt/legate && \\
-            cd /opt/legate && \\
-            python ./install.py $debug \\
-            --cuda --with-cuda /usr/local/cuda --arch volta \\
-            --with-openblas /opt/conda/envs/rapids/
-        WORKDIR /home""",
-        # TODO arch!!
-        user=os.getuid(),
-        debug='--debug' if is_debug else '',
-        legate_branch=legate_branch)
+#    writer.emit("""
+#        RUN source activate rapids && \\
+#            echo "Change this message for re-build!" && \\
+#            git clone -b $legate_branch https://gitlab-master.nvidia.com/mjoux/Legate.git \\
+#            /opt/legate && \\
+#            cd /opt/legate && \\
+#            python ./install.py $debug \\
+#            --cuda --with-cuda /usr/local/cuda --arch volta \\
+#            --with-openblas /opt/conda/envs/rapids/
+#        WORKDIR /home""",
+#        # TODO arch!!
+#        user=os.getuid(),
+#        debug='--debug' if is_debug else '',
+#        legate_branch=legate_branch)
