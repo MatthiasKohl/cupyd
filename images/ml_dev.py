b0af8060130c0e245d9a3b15e0729a823f3c4d0d
Matthias Jouanneaux <mjoux@dlcluster.nvidia.com>
2020-04-21 01:43:08 -0700

    added newer gcc

--- images/ml_dev.py
+++ images/ml_dev.py
@@ -18,7 +18,16 @@ def emit(writer, **kwargs):
     modules.conda_ml_env.emit(writer)
     modules.openmpi.emit(writer, devBuild=False, ompiVersion="4.0.2")
     modules.cuml_dev.emit(writer, **kwargs)
-
+    writer.packages(['software-properties-common'])
+    writer.emit("""
+        RUN add-apt-repository ppa:ubuntu-toolchain-r/test
+    """)
+    writer.packages(['gcc-7', 'g++-7'])
+    writer.emit("""
+        RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 90 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+        ENV CC gcc-7
+        ENV CXX g++-7
+    """)
 
 def get_params(rapidsVersion, cudaVersionFull):
     return {"cudaVersionFull": cudaVersionFull,
