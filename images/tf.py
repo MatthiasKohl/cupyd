3cfa5fae126106dd7225b67c52dd9045058176b4
Matthias Jouanneaux <mjoux@dlcluster.nvidia.com>
2020-04-22 07:07:52 -0700

    modified to add TF1 and to work with GC VCD code

--- images/tf.py
+++ images/tf.py
@@ -5,14 +5,24 @@ def emit(writer, **kwargs):
     # everything is already installed, just add runas and ssh
     modules.runas.emit(writer)
     modules.ssh.emit(writer)
+    assert 'TFMajorVersion' in kwargs
+    tfp_install = 'tensorflow-probability'
+    if kwargs['TFMajorVersion'] == 1:
+        tfp_install = 'tensorflow-probability==0.7'
     writer.emit("""
-        RUN pip install --upgrade tensorflow-probability
-    """)
+        RUN pip install $tfp_install fs pendulum
+    """, tfp_install=tfp_install)
 
 def images():
     return {
         "tf:20.03": {
             "base": "nvcr.io/nvidia/tensorflow:20.03-tf2-py3 ",
+            "TFMajorVersion": 2,
+            "needsContext": True,
+        },
+        "tf1:20.03": {
+            "base": "nvcr.io/nvidia/tensorflow:20.03-tf1-py3 ",
+            "TFMajorVersion": 1,
             "needsContext": True,
         },
     }
